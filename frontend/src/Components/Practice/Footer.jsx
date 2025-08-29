a2444103fc9871e6180c1b97c923f92948fe5293
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-08-29 10:46:24 +0545

    Footer Added

---
+++ frontend/src/Components/Practice/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='bg-gray-100 text-center p-4 mt-10'>
+            <p className='text-sm text-gray-600'>© 2025 TourNepal. All rights reserved.</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer;
